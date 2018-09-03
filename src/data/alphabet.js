34827b0b2733945ecee21f7c1b8986f9c25e02fb
Alison Major <alison.major@gmail.com>
2018-09-03 17:03:31 -0400

    Add letter images

--- src/data/alphabet.js
+++ src/data/alphabet.js
@@ -1,61 +1,112 @@
+import letterA from './images/alphabet/a.jpg';
+import letterB from './images/alphabet/b.jpg';
+import letterC from './images/alphabet/c.jpg';
+import letterD from './images/alphabet/d.jpg';
+import letterE from './images/alphabet/e.jpg';
+import letterF from './images/alphabet/f.jpg';
+import letterG from './images/alphabet/g.jpg';
+import letterH from './images/alphabet/h.jpg';
+import letterI from './images/alphabet/i.jpg';
+import letterJ from './images/alphabet/j.gif';
+import letterK from './images/alphabet/k.jpg';
+import letterL from './images/alphabet/l.jpg';
+import letterM from './images/alphabet/m.jpg';
+import letterN from './images/alphabet/n.jpg';
+import letterO from './images/alphabet/o.jpg';
+import letterP from './images/alphabet/p.jpg';
+import letterQ from './images/alphabet/q.jpg';
+import letterR from './images/alphabet/r.jpg';
+import letterS from './images/alphabet/s.jpg';
+import letterT from './images/alphabet/t.jpg';
+import letterU from './images/alphabet/u.jpg';
+import letterV from './images/alphabet/v.jpg';
+import letterW from './images/alphabet/w.jpg';
+import letterX from './images/alphabet/x.jpg';
+import letterY from './images/alphabet/y.jpg';
+import letterZ from './images/alphabet/z.gif';
+
 const aslAlphabet = () => ({
   name: 'Alphabet',
   description: 'The alphabet.',
   vocabulary: [
     {
       term: 'A',
-      image: 'https://usercontent1.hubstatic.com/8683028.png',
-      notes: 'Hints about signing the letter A',
+      image: letterA,
     }, {
       term: 'B',
+      image: letterB,
     }, {
       term: 'C',
+      image: letterC,
     }, {
       term: 'D',
+      image: letterD,
     }, {
       term: 'E',
+      image: letterE,
     }, {
       term: 'F',
+      image: letterF,
     }, {
       term: 'G',
+      image: letterG,
     }, {
       term: 'H',
+      image: letterH,
     }, {
       term: 'I',
+      image: letterI,
     }, {
       term: 'J',
+      image: letterJ,
     }, {
       term: 'K',
+      image: letterK,
     }, {
       term: 'L',
+      image: letterL,
     }, {
       term: 'M',
+      image: letterM,
     }, {
       term: 'N',
+      image: letterN,
     }, {
       term: 'O',
+      image: letterO,
     }, {
       term: 'P',
+      image: letterP,
     }, {
       term: 'Q',
+      image: letterQ,
     }, {
       term: 'R',
+      image: letterR,
     }, {
       term: 'S',
+      image: letterS,
     }, {
       term: 'T',
+      image: letterT,
     }, {
       term: 'U',
+      image: letterU,
     }, {
       term: 'V',
+      image: letterV,
     }, {
       term: 'W',
+      image: letterW,
     }, {
       term: 'X',
+      image: letterX,
     }, {
       term: 'Y',
+      image: letterY,
     }, {
       term: 'Z',
+      image: letterZ,
     },
   ],
 });

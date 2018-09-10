99a31b71d4ff577045a63863b2a77a25a0ef8dc9
Alison Major <alison.major@gmail.com>
2018-09-10 00:47:05 -0400

    Add classifier data

---
+++ src/data/classifiers.js
@@ -0,0 +1,57 @@
+const aslClassifiers = () => ({
+  name: 'Classifiers',
+  description: 'Basic classifer terms.',
+  vocabulary: [
+    {
+      term: 'Small, flat, and round objects.',
+      image: 'http://asl.amajorendeavor.com/classifiers/temp-classifier-f.png',
+      notes: [
+        'Examples: coin, button, a watch',
+      ],
+    }, {
+      term: 'Flat and round objects.',
+      image: 'http://asl.amajorendeavor.com/classifiers/temp-classifier-ll-small.png',
+      notes: [
+        'Examples: a pancake, a small dish or plate, a hamburger',
+      ],
+    }, {
+      term: 'Larger flat and round objects.',
+      image: 'http://asl.amajorendeavor.com/classifiers/temp-classifier-ll-large.png',
+      notes: [
+        'Examples: a large plate, a big steak, a large puddle',
+      ],
+    }, {
+      term: 'Small container-like objects',
+      image: 'http://asl.amajorendeavor.com/classifiers/temp-classifier-c.png',
+      notes: [
+        'Examples: a cup, a glass, a bottle, a vase, a can',
+      ],
+    }, {
+      term: 'Container-like objects',
+      image: 'http://asl.amajorendeavor.com/classifiers/temp-classifier-cc-small.png',
+      notes: [
+        'Examples: a bowl, a large can, a thick cable',
+      ],
+    }, {
+      term: 'Larger container-like objects',
+      image: 'http://asl.amajorendeavor.com/classifiers/temp-classifier-cc-large.png',
+      notes: [
+        'Examples: a pail, a large bowl, a hat box',
+      ],
+    }, {
+      term: 'Objects with a flat surface',
+      image: 'http://asl.amajorendeavor.com/classifiers/temp-classifier-bb-small.png',
+      notes: [
+        'Examples: a small table, a shelf',
+      ],
+    }, {
+      term: 'Longer objects with a flat surface',
+      image: 'http://asl.amajorendeavor.com/classifiers/temp-classifier-bb-long.png',
+      notes: [
+        'Examples: a long board, a long shelf, a long table, a plot of land',
+      ],
+    },
+  ],
+});
+
+export { aslClassifiers as default };

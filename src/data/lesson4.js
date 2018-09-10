94c79a1cf3fbf189c1d699fc58c1e90b3ebc630a
Alison Major <alison.major@gmail.com>
2018-09-09 23:52:46 -0400

    Add lesson 4 data

---
+++ src/data/lesson4.js
@@ -0,0 +1,169 @@
+const aslLesson4 = () => ({
+  name: 'Lesson 4',
+  description: 'Vocabulary words from Lesson 4.',
+  vocabulary: [
+    {
+      term: 'AWFUL, terrible',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-awful.png',
+    }, {
+      term: 'BAD, unfortunate',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-bad.png',
+    }, {
+      term: 'BELIEVE',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-believe.png',
+    }, {
+      term: 'CAT',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-cat.png',
+    }, {
+      term: 'CHEAP',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-cheap.png',
+    }, {
+      term: 'CLEAN, NICE, pure',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-clean.png',
+    }, {
+      term: 'COLD, WINTER',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-cold.png',
+    }, {
+      term: 'DIRTY',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-dirty.png',
+    }, {
+      term: 'DOG',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-dog.png',
+    }, {
+      term: 'DON\'T KNOW',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-dont-know.png',
+    }, {
+      term: 'DON\'T LIKE',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-dont-like.png',
+    }, {
+      term: 'DON\'T WANT',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-dont-want.png',
+    }, {
+      term: 'EAT, FOOD',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-eat.png',
+    }, {
+      term: 'EXPENSIVE',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-expensive.png',
+    }, {
+      term: 'EXPLAIN, describe',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-explain.png',
+      notes: [
+        'Use F-hands.',
+      ],
+    }, {
+      term: 'FEEL, feelings, sense',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-feel.png',
+      notes: [
+        'Middle finger sweeps up the sternum.',
+      ],
+    }, {
+      term: 'GOOD',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-good.png',
+      note: [
+        'Signed the same as "Thank You"',
+      ],
+    }, {
+      term: 'HEAR, sound',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-hear.png',
+    }, {
+      term: 'HOT, heat',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-hot.png',
+    }, {
+      term: 'LANGUAGE',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-language.png',
+      notes: [
+        'L-hands, wiggle out.',
+      ],
+    }, {
+      term: 'LESSON, COURSE, chapter',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-lesson.png',
+    }, {
+      term: 'LOVE',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-love.png',
+      notes: [
+        'Example: Love people, love your dog.',
+      ],
+    }, {
+      term: 'MEET',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-meet.png',
+    }, {
+      term: 'MOVIE',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-movie.png',
+    }, {
+      term: 'NO',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-no.png',
+    }, {
+      term: 'NOT',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-not.png',
+    }, {
+      term: 'OH I SEE, oh',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-oh-i-see.png',
+    }, {
+      term: 'RIGHT, correct',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-right.png',
+    }, {
+      term: 'WRONG',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-wrong.png',
+    }, {
+      term: 'SCHOOL',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-school.png',
+      notes: [
+        'Tap twice.',
+      ],
+    }, {
+      term: 'SEE, sight',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-see.png',
+    }, {
+      term: 'SENTENCE',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-sentence.png',
+      notes: [
+        'F-hands, wiggle out your dominant hand.',
+      ],
+    }, {
+      term: 'SICK',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-sick.png',
+    }, {
+      term: 'SMELL, odor',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-smell.png',
+    }, {
+      term: 'SORRY, regret',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-sorry.png',
+      notes: [
+        'Use S-hand.',
+      ],
+    }, {
+      term: 'TASTE, prefer, favorite',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-taste.png',
+      notes: [
+        'Taste, touch the tip of your tongue with your middle finger.',
+        'Prefer or favorite, touch your chin with your middle finger.',
+      ],
+    }, {
+      term: 'UNDERSTAND, comprehend',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-understand.png',
+    }, {
+      term: 'WELL, so',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-well.png',
+    }, {
+      term: 'WORD, vocabulary',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-word.png',
+    }, {
+      term: 'WORK, JOB',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-work.png',
+    }, {
+      term: 'WORLD',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-world.png',
+      notes: [
+        'Dominant hand goes around once.',
+      ],
+    }, {
+      term: 'YES',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-yes.png',
+    }, {
+      term: 'YOUNG, youth',
+      image: 'http://asl.amajorendeavor.com/lesson-4/temp-young.png',
+    },
+  ],
+});
+
+export { aslLesson4 as default };

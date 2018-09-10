0e3da282e1772d1d5c283c79c84ed5bf08981c85
Alison Major <alison.major@gmail.com>
2018-09-09 23:40:13 -0400

    Add lesson 3 data

---
+++ src/data/lesson3.js
@@ -0,0 +1,165 @@
+const aslLesson3 = () => ({
+  name: 'Lesson 3',
+  description: 'Vocabulary words from Lesson 3.',
+  vocabulary: [
+    {
+      term: 'ABOUT, concerning',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-about.png',
+    }, {
+      term: 'BED',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-bed.png',
+    }, {
+      term: 'BOOK',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-book.png',
+      notes: [
+        'Motion twice.',
+      ],
+    }, {
+      term: 'BOX, package, room',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-box.png',
+    }, {
+      term: 'CAR',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-car.png',
+    }, {
+      term: 'CHAIR, seat',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-chair.png',
+      notes: [
+        'Tap twice.',
+      ],
+    }, {
+      term: 'CLASS, GROUP, team',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-class.png',
+      notes: [
+        'Use C-hands.',
+      ],
+    }, {
+      term: 'DIALOGUE, talk with',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-dialogue.png',
+    }, {
+      term: 'ENJOY, appreciate',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-enjoy.png',
+      notes: [
+        'Use both hands.',
+      ],
+    }, {
+      term: 'FIND, discover',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-find.png',
+    }, {
+      term: 'FINGERSPELL',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-fingerspell.png',
+    }, {
+      term: 'FOR',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-for.png',
+    }, {
+      term: 'FORGET',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-forget.png',
+    }, {
+      term: 'FROM',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-from.png',
+    }, {
+      term: 'HAVE, own, possess',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-have.png',
+    }, {
+      term: 'HOME',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-home.png',
+      notes: [
+        'Where you "eat" and where you "sleep."',
+      ],
+    }, {
+      term: 'IN, contained in',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-in.png',
+    }, {
+      term: 'KNOW, aware, conscious',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-know.png',
+    }, {
+      term: 'LIKE',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-like.png',
+    }, {
+      term: 'LOSE',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-lose.png',
+    }, {
+      term: 'MONEY, financial, economic',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-money.png',
+    }, {
+      term: 'NAME',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-name.png',
+      notes: [
+        'Double tap.',
+      ],
+    }, {
+      term: 'NEED, necessary, should, must',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-need.png',
+    }, {
+      term: 'NEW, modern',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-new.png',
+    }, {
+      term: 'OLD, age',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-old.png',
+      notes: [
+        'Like pulling on a long beard.',
+      ],
+    }, {
+      term: 'PAPER, page',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-paper.png',
+      notes: [
+        'Swipe palms twice.',
+      ],
+    }, {
+      term: 'PRACTICE, exercise, train, rehearse',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-practice.png',
+    }, {
+      term: 'READ',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-read.png',
+    }, {
+      term: 'REMEMBER',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-remember.png',
+    }, {
+      term: 'SIGN, sign language',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-sign.png',
+    }, {
+      term: 'TABLE, desk',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-table.png',
+    }, {
+      term: 'THANK-YOU',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-thankyou.png',
+    }, {
+      term: 'WANT, desire',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-want.png',
+    }, {
+      term: 'COLOR',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-color.png',
+    }, {
+      term: 'BLACK',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-color-black.png',
+    }, {
+      term: 'WHITE',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-color-white.png',
+    }, {
+      term: 'BLUE',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-color-blue.png',
+    }, {
+      term: 'BROWN',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-color-brown.png',
+    }, {
+      term: 'GRAY',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-color-gray.png',
+    }, {
+      term: 'GREEN',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-color-green.png',
+    }, {
+      term: 'ORANGE',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-color-orange.png',
+      notes: [
+        'Squeeze the "O" towards your mouth, like squirting orange juice.',
+      ],
+    }, {
+      term: 'RED',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-color-red.png',
+    }, {
+      term: 'YELLOW',
+      image: 'http://asl.amajorendeavor.com/lesson-3/temp-color-yellow.png',
+    },
+  ],
+});
+
+export { aslLesson3 as default };

24ee09a279cbbd0db3058a02a42d97f73551ad25
Alison Major <alison.major@gmail.com>
2018-09-09 23:25:04 -0400

    Add lesson 2 data

---
+++ src/data/lesson2.js
@@ -0,0 +1,178 @@
+const aslLesson2 = () => ({
+  name: 'Lesson 2',
+  description: 'Vocabulary words from Lesson 2.',
+  vocabulary: [
+    {
+      term: 'MAN',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-man.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'WOMAN',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-woman.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'BOY, man',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-boy.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+        'Like grabbing the bill of a ball cap.',
+      ],
+    }, {
+      term: 'GIRL',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-girl.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+        'Like tracing the string of a bonnet.',
+      ],
+    }, {
+      term: 'FAMILY',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-family.png',
+    }, {
+      term: 'HUSBAND',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-husband.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'WIFE',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-wife.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'GRANDFATHER',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-grandfather.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'GRANDMOTHER',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-grandmother.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'FATHER',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-father.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'MOTHER',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-mother.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'PARENTS',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-parents.png',
+    }, {
+      term: 'CHILD, short',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-child.png',
+    }, {
+      term: 'SON',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-son.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'DAUGHTER',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-daughter.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'BROTHER',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-brother.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'SISTER',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-sister.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'UNCLE',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-uncle.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+        'Hand in the shape of a "U"',
+      ],
+    }, {
+      term: 'AUNT',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-aunt.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+        'Hand in the shape of an "A"',
+      ],
+    }, {
+      term: 'NEPHEW',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-nephew.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'NIECE',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-niece.png',
+      notes: [
+        'Male pronouns are signed above the nose, female pronouns are signed below the nose.',
+      ],
+    }, {
+      term: 'FRIEND',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-friend.png',
+    }, {
+      term: 'GOOD-FRIEND',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-good-friend.png',
+    }, {
+      term: 'AMERICA',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-america.png',
+    }, {
+      term: 'COOK, bake',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-cook.png',
+    }, {
+      term: 'DANCE, party',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-dance.png',
+    }, {
+      term: 'HIS, HERS, ITS',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-his-hers-its.png',
+    }, {
+      term: 'LEARN, acquire',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-learn.png',
+    }, {
+      term: 'MY, mine',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-my.png',
+    }, {
+      term: 'OUR, ours',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-our.png',
+    }, {
+      term: 'PLEASE',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-please.png',
+    }, {
+      term: 'STUDENT',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-student.png',
+    }, {
+      term: 'TEACH, educate',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-teach.png',
+    }, {
+      term: 'TEACHER',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-teacher.png',
+    }, {
+      term: 'THEIR, theirs',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-their.png',
+    }, {
+      term: 'YOUR, yours (singular)',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-your-singular.png',
+    }, {
+      term: 'YOUR, yours (plural)',
+      image: 'http://asl.amajorendeavor.com/lesson-2/temp-your-plural.png',
+    },
+  ],
+});
+
+export { aslLesson2 as default };

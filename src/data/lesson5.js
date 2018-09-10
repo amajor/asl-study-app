aeb53d4382de555ad2410c0c0762c662a00a1dd3
Alison Major <alison.major@gmail.com>
2018-09-10 00:12:37 -0400

    Add lesson 5 data

---
+++ src/data/lesson5.js
@@ -0,0 +1,231 @@
+const aslLesson5 = () => ({
+  name: 'Lesson 5',
+  description: 'Vocabulary words from Lesson 5.',
+  vocabulary: [
+    {
+      term: 'AFTER AWHILE, later',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-after-awhile.png',
+      notes: [
+        'L-hand, thumb on non-dominant, fore-finger tips forward.',
+      ],
+    }, {
+      term: 'AFTERNOON',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-afternoon.png',
+    }, {
+      term: 'ALL DAY',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-all-day.png',
+    }, {
+      term: 'ALL NIGHT, overnight',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-all-night.png',
+    }, {
+      term: 'BEFORE, previous',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-before.png',
+    }, {
+      term: 'BUY',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-buy.png',
+    }, {
+      term: 'COLLEGE',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-college.png',
+      note: [
+        'Think about how school is free (no height between your hands), but then college is a big tuition, a big stack of money between your hands.',
+      ],
+    }, {
+      term: 'COOKIE',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-cookie.png',
+      notes: [
+        'C-hand used like a cookie cutter on your non-dominant hand.',
+      ],
+    }, {
+      term: 'DAY',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-day.png',
+    }, {
+      term: 'DIE, dead, death',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-die.png',
+      notes: [
+        'Rolling over in grave...',
+        'Start with non-dominant up, dominant down. Then roll hands over.',
+      ],
+    }, {
+      term: 'DOCTOR',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-doctor.png',
+    }, {
+      term: 'DRINK',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-drink.png',
+    }, {
+      term: 'FINE',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-fine.png',
+    }, {
+      term: 'FINISH, already, then',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-finish.png',
+    }, {
+      term: 'FUTURE',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-future.png',
+    }, {
+      term: 'GO AWAY, leave',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-go-away.png',
+    }, {
+      term: 'GROW UP',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-grow-up.png',
+    }, {
+      term: 'HOMEWORK',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-homework.png',
+    }, {
+      term: '1-HOUR, hour',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-1-hour.png',
+      notes: [
+        'Non-dominant hand is like a clock face.',
+        'Dominant hand is a minute hand, going around the clock a full cycle.',
+      ],
+    }, {
+      term: 'IMPROVE',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-improve.png',
+    }, {
+      term: 'LATER',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-later.png',
+    }, {
+      term: 'LETTER, mail',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-letter.png',
+      notes: [
+        'Like licking a stamp and putting it on an envelope.',
+      ],
+    }, {
+      term: 'LONG AGO',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-long-ago.png',
+      notes: [
+        'Open hand, motioning towards behind you.',
+      ],
+    }, {
+      term: 'MAKE',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-make.png',
+      notes: [
+        'Non-dominant hand is stationary.',
+        'Dominant hand starts towards you, then bounces to face out.',
+      ],
+    }, {
+      term: '1-MINUTE, minute',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-1-minute.png',
+      notes: [
+        'Non-dominant hand is like a clock face.',
+        'Dominant hand is a minute hand, ticks one minute.',
+      ],
+    }, {
+      term: '1-MONTH, month',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-1-month.png',
+      notes: [
+        'Non-dominant hand is like a hanging calendar, pointing up.',
+        'Dominant hand is behind the finger of the non-dominant hand, moving down.',
+      ],
+    }, {
+      term: 'MORNING',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-morning.png',
+    }, {
+      term: 'NIGHT, TONIGHT',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-night.png',
+    }, {
+      term: 'NOON',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-noon.png',
+    }, {
+      term: 'NOW',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-now.png',
+      notes: [
+        'Drop Y-hands to your waist.',
+      ],
+    }, {
+      term: 'PANTS',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-pants.png',
+    }, {
+      term: 'RECENTLY, just awhile ago',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-recently.png',
+    }, {
+      term: 'RESIDENTIAL SCHOOL',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-residential-school.png',
+      notes: [
+        'I-hands, tap twice.',
+      ],
+    }, {
+      term: 'SELL',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-sell.png',
+      notes: [
+        'Squishy hands, flick out.',
+      ],
+    }, {
+      term: 'STAY',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-stay.png',
+    }, {
+      term: 'TODAY',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-today.png',
+      notes: [
+        'Same sign as "now."',
+      ],
+    }, {
+      term: 'TO TELEPHONE, call',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-to-telephone.png',
+    }, {
+      term: 'TOMORROW',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-tomorrow.png',
+    }, {
+      term: 'USED TO, formerly',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-used-to.png',
+      note: [
+        'Spin your hand towards the back.',
+        'It\'s in the past, so it motions behind you.',
+      ],
+    }, {
+      term: 'VISIT',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-visit.png',
+    }, {
+      term: 'WALK',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-walk.png',
+    }, {
+      term: 'WALK',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-walk2.png',
+      notes: [
+        'Use 3-hands.',
+      ],
+    }, {
+      term: 'WASHINGTON',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-washington.png',
+      notes: [
+        'Use w-hand on shoulder, twirling out.',
+      ],
+    }, {
+      term: 'WASHINGTON D.C.',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-washington.png',
+      notes: [
+        'Use w-hand on shoulder, twirling out, followed by fingerspelling "D" and "C"',
+      ],
+    }, {
+      term: 'WATER',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-water.png',
+    }, {
+      term: 'WEAR, use',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-wear.png',
+      notes: [
+        'Use U-hands.',
+      ],
+    }, {
+      term: '1-WEEK, week',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-1-week.png',
+    }, {
+      term: 'WILL, shall, future',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-will.png',
+      notes: [
+        'This is an affirmation of your plans.',
+      ],
+    }, {
+      term: 'WRITE',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-write.png',
+    }, {
+      term: 'YEAR',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-year.png',
+      notes: [
+        'Dominant fist roatates around non-dominant fist once.',
+      ],
+    }, {
+      term: 'YESTERDAY',
+      image: 'http://asl.amajorendeavor.com/lesson-5/temp-yesterday.png',
+    },
+  ],
+});
+
+export { aslLesson5 as default };

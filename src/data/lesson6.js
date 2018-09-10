9fa9508795db124faed59df079a6b4b9aa224191
Alison Major <alison.major@gmail.com>
2018-09-10 00:22:44 -0400

    Add lesson 6 data

---
+++ src/data/lesson6.js
@@ -0,0 +1,168 @@
+const aslLesson6 = () => ({
+  name: 'Lesson 6',
+  description: 'Vocabulary words from Lesson 6.',
+  vocabulary: [
+    {
+      term: 'ADDRESS',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-address.png',
+      notes: [
+        'Where you live.',
+      ],
+    }, {
+      term: 'AGAIN, repeat',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-again.png',
+    }, {
+      term: 'ANSWER, respond',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-answer.png',
+    }, {
+      term: 'ASK',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-ask.png',
+    }, {
+      term: 'BABY',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-baby.png',
+    }, {
+      term: 'BICYCLE',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-bicycle.png',
+    }, {
+      term: 'BOTTLE',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-bottle.png',
+    }, {
+      term: 'BREAD',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-bread.png',
+      notes: [
+        'Slice the "bread" with your hand.',
+      ],
+    }, {
+      term: 'CHALLENGE, play against',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-challenge.png',
+      notes: [
+        'Toward your non-dominant hand.',
+      ],
+    }, {
+      term: 'CLOTHES, DRESS, wear',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-clothes.png',
+    }, {
+      term: 'COFFEE',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-coffee.png',
+    }, {
+      term: 'COKE',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-coke-drug.png',
+    }, {
+      term: 'DEPEND',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-depend.png',
+    }, {
+      term: 'DRIVE',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-drive.png',
+    }, {
+      term: 'FORCE',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-force.png',
+    }, {
+      term: 'FUNNY, amusing',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-funny.png',
+    }, {
+      term: 'GIVE',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-give.png',
+    }, {
+      term: 'GLASS, CAN, cup',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-glass.png',
+    }, {
+      term: 'HATE',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-hate.png',
+    }, {
+      term: 'HELP, aid',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-help.png',
+    }, {
+      term: 'INFLUENCE, affect',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-influence.png',
+    }, {
+      term: 'INFORM, notify, obey',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-inform.png',
+    }, {
+      term: 'INTRODUCE',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-introduce.png',
+    }, {
+      term: 'LOOK AT, WATCH',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-look-at.png',
+    }, {
+      term: 'MEAT, steak',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-meat.png',
+    }, {
+      term: 'MILK',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-milk.png',
+    }, {
+      term: 'PAY',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-pay.png',
+    }, {
+      term: 'PICTURE, photograph',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-picture.png',
+    }, {
+      term: 'PLANT, spring',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-plant.png',
+    }, {
+      term: 'PROUD, pride',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-proud.png',
+    }, {
+      term: 'REQUEST, ask',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-request.png',
+    }, {
+      term: 'SEND, mail to',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-send.png',
+    }, {
+      term: 'SHOW, demonstrate, illustrate',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-show.png',
+    }, {
+      term: 'SHY',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-shy.png',
+    }, {
+      term: 'CUTE',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-cute.png',
+    }, {
+      term: 'SWEET',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-sweet.png',
+    }, {
+      term: 'TEA',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-tea.png',
+    }, {
+      term: 'TELETYPEWRITER',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-teletypewriter.png',
+    }, {
+      term: 'TELL',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-tell.png',
+    }, {
+      term: 'TICKET',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-ticket.png',
+    }, {
+      term: 'UMBRELLA',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-umbrella.png',
+    }, {
+      term: 'WAIT',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-wait.png',
+    }, {
+      term: 'WRIST WATCH',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-wristwatch.png',
+    }, {
+      term: 'SUNDAY',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-days-1-sunday.png',
+    }, {
+      term: 'MONDAY',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-days-2-monday.png',
+    }, {
+      term: 'TUESDAY',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-days-3-tuesday.png',
+    }, {
+      term: 'WEDNESDAY',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-days-4-wednesday.png',
+    }, {
+      term: 'THURSDAY',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-days-5-thursday.png',
+    }, {
+      term: 'FRIDAY',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-days-6-friday.png',
+    }, {
+      term: 'SATURDAY',
+      image: 'http://asl.amajorendeavor.com/lesson-6/temp-days-7-saturday.png',
+    },
+  ],
+});
+
+export { aslLesson6 as default };
